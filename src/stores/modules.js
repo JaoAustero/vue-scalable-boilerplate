437978a1e73b347115cd36549df919232ad82968
Jerald Austero <jerald.austero0229@gmail.com>
2020-06-05 14:37:48 +0800

    🎨 Improve the store auto register
    We improve the structure of the store auto-register
    file.

--- src/stores/modules.js
+++ src/stores/modules.js
@@ -1,54 +1,77 @@
+/**
+ * @file Register each file as corresponding Vuex module. It will
+ * search in the whole application with file name `store.js`.
+ */
+
 const modulesCache = {};
 const storeData = { modules: {} };
 const fs = require('.');
 
 (function updateModules()
 {
-    // Allow us to dynamically require all Vuex module files.
-    // https://webpack.js.org/guides/dependency-management/#require-context
+    /**
+     * Allow us to dynamically require all Vuex module files.
+     *
+     * @see {@link https://webpack.js.org/guides/dependency-management/#require-context}
+     */
     const requireModule = require.context(
-        // Search for files in the current directory.
+        /**
+         * Search for files in the current directory.
+         */
         '@/views/',
-        // Search for files in subdirectories.
+        /**
+         * Search for files in subdirectories.
+         */
         true,
-        // Include any .js files that are not this file or a unit test.
-        // /^((?!index|\.unit\.).)*\.js$/
+        /**
+         * Search `store.js` in all directories inside of this app.
+         */
         /store.js/
     );
 
-    // For every Vuex module...
+    /**
+     * For every Vuex module.
+     */
     requireModule.keys().forEach((fileName) =>
     {
         const moduleDefinition = requireModule(fileName);
 
-        // Skip the module during hot reload if it refers to the
-        // same module definition as the one we have cached.
+        /**
+         * Skip the module during hot reload if it refers to the same module
+         * definition as the one we have cached.
+         */
         if (modulesCache[fileName] === moduleDefinition) return;
 
-        // Update the module cache, for efficient hot reloading.
+        /**
+         * Update the module cache, for efficient hot reloading.
+         */
         modulesCache[fileName] = moduleDefinition;
 
+        /**
+         * Add the module to our modules object.
+         */
         storeData.modules = Object.assign({ ...moduleDefinition }, storeData.modules);
     });
 
-    // If the environment supports hot reloading...
+    /**
+     * If the environment supports hot reloading
+     */
     if (module.hot)
     {
         // Whenever any Vuex module is updated...
         module.hot.accept(requireModule.id, () =>
         {
-            // Update `storeData.modules` with the latest definitions.
+            /**
+             * Update `storeData.modules` with the latest definitions.
+             */
             updateModules();
 
-            // Trigger a hot update in the store.
+            /**
+             * Trigger a hot update in the store.
+             */
             fs.default.hotUpdate({ modules: storeData.modules });
         });
     }
 }());
-
-if (process.env.APP_DEBUG)
-{
-    // console.log(storeData.modules);
-}
 
 export default storeData.modules;
