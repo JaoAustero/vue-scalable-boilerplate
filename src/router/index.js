9c5406ba484f1e4c1904221c9d5064ee6026da89
Jerald Austero <jerald.austero@gigamare.com>
2020-06-06 21:46:01 +0800

    💡 Vue Routes
    We adde a code documentation in the auto-register routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
+/**
+ * @file A main file for the vue router
+ */
 import Vue from 'vue';
 import Router from 'vue-router';
-// import { Cookie } from '@/utils/cookie';
 
 import routes from './routes';
 
@@ -9,31 +11,6 @@ Vue.use(Router);
 export const router = new Router({
     mode: 'history',
     transitionOnLoad: true,
-    linkActiveClass: 'uis-active',
-    linkExactActiveClass: 'uis-active',
     root: '/',
     routes,
 });
-
-// router.beforeEach((to, from, next) =>
-// {
-//     const notAuthRequired = [
-//         'signin',
-//         'forgot-password',
-//         'activation',
-//         'reset-password',
-//         'validate-code',
-//     ];
-
-//     const authRequired = !notAuthRequired.includes(to.meta.page);
-
-//     const hasAccessToken = Cookie.get(process.env.ACCESS_TOKEN) !== null;
-
-//     if (authRequired && !hasAccessToken) return next('/signin');
-
-//     if (!authRequired && hasAccessToken) return next('/');
-
-//     if (to.path === '/') return next('/instructions');
-
-//     return next();
-// });
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,34 +1,29 @@
-import { store } from '@/stores';
+/**
+ * @file A auto-register of page routes, This will compile all `routes.js` and
+ * output a single file of routes.
+ */
 
 const routesData = [];
 
 (function updateModules()
 {
     const requireModule = require.context(
-        // Search for files in the current directory.
+        /**
+         * Search for files in the current directory.
+         */
         '@/views/',
-        // Search for files in subdirectories.
+
+        /**
+         * Search for files in subdirectories.
+         */
         true,
-        // Include any .js files that are not this file or a unit test.
-        // /^((?!index|\.unit\.).)*\.js$/
+        
+        /**
+         * Include any .js files that are not this file or a unit test.
+         */
         /route.js/
     );
 
-    const root = {
-        path: '/',
-        name: 'Root',
-        component: () => import('@/views/auth'),
-        beforeEnter: (to, from, next) =>
-        {
-            const isActive = store.getters['auth/isActive'];
-
-            if (isActive) next();
-        },
-    };
-
-    routesData.push(root);
-
-    // For every Vuex module...
     requireModule.keys().forEach((fileName) =>
     {
         const moduleDefinition = requireModule(fileName);
