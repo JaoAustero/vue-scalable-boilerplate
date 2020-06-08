4f946880e47497c7ebd2a78305bb47b0f313b3bf
Jerald Austero <jerald.austero0229@gmail.com>
2020-06-08 22:47:11 +0800

    🚩 Scroll behavior
    We added a scroll behavior when navigating to new route or
    back/forward buton we will reset the scroll into top 0, and
    added a code documentation.

--- src/router/index.js
+++ src/router/index.js
@@ -15,8 +15,37 @@ Vue.use(VueMeta, {
 });
 
 export const router = new Router({
-    mode: 'history',
-    transitionOnLoad: true,
-    root: '/',
+    /**
+     * Collection of routes in the whole application
+     */
     routes,
+
+    /**
+     * We get rid of the Hash(#) to make it more readable
+     */
+    mode: 'history',
+
+    /**
+     * Make the transition active when we try to navigate into new route.
+     */
+    transitionOnLoad: true,
+
+    /**
+     * Set the default root when the application was initialized.
+     */
+    root: '/',
+
+    /**
+     * We added a @this scrollBehavior for native scroll behavior to a new
+     * route and using back/forward buttons.
+     *
+     * @param {*} savedPosition - Current position
+     */
+    scrollBehavior(savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { x: 0, y: 0 }
+        }
+    },
 });
